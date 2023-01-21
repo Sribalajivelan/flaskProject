af85be4957bf0efdb4c0eb6025c8d3ac450633d0
sribalajivelan-icanio <sribalajivelan.s@icanio.com>
2023-01-21 10:26:04 +0530

    Added JWT

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
+import datetime
+
 from flask import Flask
+from flask_jwt_extended import JWTManager
 
 from controllers.UserController import user_api
 
 app = Flask(__name__)
 app.register_blueprint(user_api, url_prefix = '/users')
+
+jwt = JWTManager(app)
+
+app.config['JWT_SECRET_KEY'] = 'Your_Secret_Key'
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(days=1)
+jwt.init_app(app)
 
 
 @app.route('/')
--- controllers/UserController.py
+++ controllers/UserController.py
@@ -1,4 +1,5 @@
-from flask import jsonify, Blueprint, Response, json
+from flask import jsonify, Blueprint, request
+from flask_jwt_extended import create_access_token, jwt_required
 
 from models.Users import User
 
@@ -7,7 +8,26 @@ user_api = Blueprint('user_api', __name__)
 
 @user_api.route('/', methods=['POST'])
 def create_user():
-    users = User.objects(email= "sribalajivelan@gmail.com").first()
+    users = User.objects(email="sribalajivelan@gmail.com").first()
     if not users:
         users = User(name='Balaji', email='sribalajivelan@gmail.com').save()
-    return jsonify({'name' : users.name})
+    return jsonify({'name': users.name})
+
+
+@user_api.route('/login', methods=['POST'])
+def login():
+    request_body = request.get_json()
+    users = User.objects(email=request_body['email']).first()
+    if not users:
+        return jsonify({'msg': 'Username Not Found'}), 404
+    access_token = create_access_token(identity=request_body['email'])  # create jwt token
+    return jsonify(access_token=access_token), 200
+
+
+@user_api.route("/email/<string:email>", methods=['GET'])
+@jwt_required()
+def get_user_by_id(email):
+    users = User.objects(email=email).first()
+    if not users:
+        return jsonify({'msg': 'Username Not Found'}), 404
+    return jsonify(result=users.to_json()), 200
